c06894fad5632d2d56e607480f56a99b5582cc00
Henrico Pelozato Maeda <henrico.maeda@gmail.com>
2022-07-22 12:09:06 -0300

    Atualizando menu principal

--- Controle de Gastos/FrmMain.Designer.cs
+++ Controle de Gastos/FrmMain.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.PicLogo = new System.Windows.Forms.PictureBox();
+            this.BtnCreate = new System.Windows.Forms.Button();
+            this.BtnRead = new System.Windows.Forms.Button();
+            this.BtnLeave = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PicLogo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,16 +43,64 @@
             this.PicLogo.Image = global::Controle_de_Gastos.Properties.Resources.Logo;
             this.PicLogo.Location = new System.Drawing.Point(21, 21);
             this.PicLogo.Name = "PicLogo";
-            this.PicLogo.Size = new System.Drawing.Size(820, 355);
+            this.PicLogo.Size = new System.Drawing.Size(857, 355);
             this.PicLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PicLogo.TabIndex = 0;
             this.PicLogo.TabStop = false;
+            // 
+            // BtnCreate
+            // 
+            this.BtnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnCreate.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.BtnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnCreate.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCreate.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnCreate.Location = new System.Drawing.Point(552, 393);
+            this.BtnCreate.Name = "BtnCreate";
+            this.BtnCreate.Size = new System.Drawing.Size(326, 152);
+            this.BtnCreate.TabIndex = 1;
+            this.BtnCreate.Text = "ADICIONAR";
+            this.BtnCreate.UseVisualStyleBackColor = false;
+            this.BtnCreate.Click += new System.EventHandler(this.BtnCreateAndRead_Click);
+            // 
+            // BtnRead
+            // 
+            this.BtnRead.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.BtnRead.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRead.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRead.Location = new System.Drawing.Point(264, 393);
+            this.BtnRead.Name = "BtnRead";
+            this.BtnRead.Size = new System.Drawing.Size(272, 152);
+            this.BtnRead.TabIndex = 2;
+            this.BtnRead.Text = "CONSULTAR";
+            this.BtnRead.UseVisualStyleBackColor = true;
+            this.BtnRead.Click += new System.EventHandler(this.BtnCreateAndRead_Click);
+            // 
+            // BtnLeave
+            // 
+            this.BtnLeave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.BtnLeave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnLeave.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLeave.Location = new System.Drawing.Point(21, 393);
+            this.BtnLeave.Name = "BtnLeave";
+            this.BtnLeave.Size = new System.Drawing.Size(227, 152);
+            this.BtnLeave.TabIndex = 3;
+            this.BtnLeave.Text = "SAIR";
+            this.BtnLeave.UseVisualStyleBackColor = true;
+            this.BtnLeave.Click += new System.EventHandler(this.BtnLeave_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 536);
+            this.ClientSize = new System.Drawing.Size(897, 566);
+            this.Controls.Add(this.BtnLeave);
+            this.Controls.Add(this.BtnRead);
+            this.Controls.Add(this.BtnCreate);
             this.Controls.Add(this.PicLogo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmMain";
@@ -63,6 +114,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox PicLogo;
+        private System.Windows.Forms.Button BtnCreate;
+        private System.Windows.Forms.Button BtnRead;
+        private System.Windows.Forms.Button BtnLeave;
     }
 }
 
--- Controle de Gastos/FrmMain.cs
+++ Controle de Gastos/FrmMain.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Controle_de_Gastos
@@ -15,6 +8,35 @@ namespace Controle_de_Gastos
         public FrmMain()
         {
             InitializeComponent();
+            PicLogo.Select();
+        }
+
+        private void BtnCreateAndRead_Click(object sender, EventArgs e)
+        {
+            PicLogo.Select();
+            var frm = new Form();
+
+            if (sender == BtnCreate) frm = new FrmCreate();
+            else if (sender == BtnRead) frm = new FrmRead();
+            
+            frm.ShowDialog();
+        }
+
+        private void BtnLeave_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            var t = "Você tem certeza que deseja finalizar?";
+            var c = "";
+            var b = MessageBoxButtons.YesNo;
+            var i = MessageBoxIcon.Question;
+
+            DialogResult r = MessageBox.Show(t, c, b, i);
+            if (r == DialogResult.Yes) BtnLeave_Click(null, e);
+            else e.Cancel = true;
         }
     }
 }
